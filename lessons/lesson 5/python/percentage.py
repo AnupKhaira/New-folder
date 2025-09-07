11153252299069b32383352d56589572d2ee113b
AnupKhaira <manpreetKhaira783@gmail.com>
2025-09-07 16:37:34 +0100

    percent

---
+++ lessons/lesson 5/python/percentage.py
@@ -0,0 +1,10 @@
+print("enter marks obtained in 4 subjects:")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
+sum = math+english+science+hindi
+print("Sum of maths,english,science and hindi")
+perc = (sum/400)*100
+print(end="Percentage mark = ")
+print(perc)
