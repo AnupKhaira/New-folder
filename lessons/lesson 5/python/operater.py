0cac09ffc828f832a86f4dc3da67fa093f546fb5
AnupKhaira <manpreetKhaira783@gmail.com>
2025-09-07 16:17:50 +0100

    operator

---
+++ lessons/lesson 5/python/operater.py
@@ -0,0 +1,11 @@
+# Storing Values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+# Finding the total of trees.
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of al the trees is: ", sum)
+average = sum/5
+print("the average of all the trees is :", average)
