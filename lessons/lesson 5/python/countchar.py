8bc1c2f963072475444a3554a63dd0a7096562f8
AnupKhaira <manpreetKhaira783@gmail.com>
2025-10-04 16:02:24 +0100

    coun

--- lessons/lesson 5/python/countchar.py
+++ lessons/lesson 5/python/countchar.py
@@ -6,4 +6,4 @@ while(i<len(string)):
     if(string[i]==char):
         count=count+1
     i=i+1
-    print("The total number of times ", char,"has occured=", count)
+print("The total number of times ", char,"has occured=", count)
