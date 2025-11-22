7fb1de1c5d4af1c9087c47e25907651d2b73af7b
AnupKhaira <manpreetKhaira783@gmail.com>
2025-11-22 16:18:27 +0000

    22/11/25

--- lessons/lesson 5/python/classparrot.py
+++ lessons/lesson 5/python/classparrot.py
@@ -9,3 +9,14 @@ print("Blu is a {}".format(blu.species))
 print("Woo is also a {}".format(woo.species))
 print("{} is {} years old".format(blu.name, blu.age))
 print("{} is {} yeras old".format(woo.name, woo.age))
+class Dog:
+    species = "Dog"
+    def_init_(self,name,age):
+    self.name=name
+    self.age=age
+blu= Dog("Laika",10)
+woo= Dog("Pomsky",15)
+print("Laika is a {}".format(blu.species))
+print("Pomsky is also a {}".format(woo.species))
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} yeras old".format(woo.name, woo.age))
